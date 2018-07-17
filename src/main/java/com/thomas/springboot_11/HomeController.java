93d6f5ccf848963357261f4c6c6d45e4bf49f904
apocalypeseed <tswlew@yahoo.com>
2018-07-16 20:34:12 -0400

    Initial commit

---
+++ src/main/java/com/thomas/springboot_11/HomeController.java
@@ -0,0 +1,14 @@
+package com.thomas.springboot_11;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController
+{
+    @RequestMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+}
